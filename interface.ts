7532d6f0a9dadd871705f3a405e6d84ff8c982ea
Nontapan Sitthichotlertpakdee <masternonnolnw@gmail.com>
2024-10-18 00:00:18 +0700

    feat: add interface

--- interface.ts
+++ interface.ts
@@ -1,19 +1,26 @@
 interface HospitalItem {
-    _id: string,
-    name: string,
-    address: string,
-    district: string,
-    province: string,
-    postalcode: string,
-    tel: string,
-    picture: string,
-    __v: number,
-    id: string
-  }
-  
-  interface HospitalJson {
-    success: boolean,
-    count: number,
-    pagination: Object,
-    data: HospitalItem[]
-  }
+  _id: string;
+  name: string;
+  address: string;
+  district: string;
+  province: string;
+  postalcode: string;
+  tel: string;
+  picture: string;
+  __v: number;
+  id: string;
+}
+
+interface HospitalJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: HospitalItem[];
+}
+
+interface SingleHospitalJson {
+  success: boolean;
+  count: number;
+  pagination: Object;
+  data: HospitalItem;
+}
